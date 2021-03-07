257a07917361ae0ec57a009d3d5321dc4a890b8b
Ibrahim Abu-awad <ibrahimabuawadwork@gmail.com>
2021-03-07 12:58:01 +0200

    add loops

--- dynamic-test.js
+++ dynamic-test.js
@@ -1,10 +1,29 @@
-
+let cat = prompt("how many cat pic do you want");
 let chColor = prompt("choose text color for FORM( blue , red , green )");
 let Name = prompt("whats your Name");
 let age = prompt("whats your Age");
 
+
 alert("Hello " + Name + " your age is " + age);
 
+let order = prompt("what do you want to Coffee or tea ?");
+
+let lowerOrder = order.toLowerCase();
+while (lowerOrder != 'coffee' && lowerOrder != 'tea') {
+
+    lowerOrder = prompt("what do you want to Coffee or tea ?").toLowerCase();
+
+}
+
+
+let imgDiv = document.getElementById('imgDiv');
+for (i = 0; i < cat; i++) {
+    let a = document.createElement('img');
+    a.setAttribute('src', "https://www.catbreedslist.com/uploads/allimg/cat-pictures/Nebelung-1.jpg");
+
+    imgDiv.appendChild(a);
+
+};
 let container = document.getElementById("container");
 let lowerColor = chColor.toLowerCase();
 
@@ -15,7 +34,9 @@ else {
     alert("you put wrong color");
 }
 
+
 function returnColor() {
 
     container.style.color = "black";
 }
+
